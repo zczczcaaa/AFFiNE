1f6cce2f5ed974c30ba017e2f3fb71e0b8b58876
EYHN <cneyhn@gmail.com>
2024-10-31 17:15:46 +0000

    fix(core): fix copy block link (#8650)

--- packages/frontend/core/src/components/affine/share-page-modal/share-menu/share-page.tsx
+++ packages/frontend/core/src/components/affine/share-page-modal/share-menu/share-page.tsx
@@ -172,8 +172,8 @@ export const AFFiNESharePage = (props: ShareMenuProps) => {
     onClickCopyLink('edgeless' as DocMode);
   }, [onClickCopyLink]);
   const onCopyBlockLink = useCallback(() => {
-    onClickCopyLink(undefined, blockIds, elementIds);
-  }, [onClickCopyLink, blockIds, elementIds]);
+    onClickCopyLink(currentMode, blockIds, elementIds);
+  }, [onClickCopyLink, currentMode, blockIds, elementIds]);
 
   if (isLoading) {
     // TODO(@eyhn): loading and error UI
