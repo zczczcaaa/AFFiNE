055fa0a8b4e1849490936c8740f2223776a2fc85
forehalo <forehalo@gmail.com>
2024-11-14 09:59:01 +0000

    fix(core): polyfill iterator helper for safari (#8824)
    fix AF-1691

--- packages/frontend/core/src/bootstrap/polyfill/browser.ts
+++ packages/frontend/core/src/bootstrap/polyfill/browser.ts
@@ -1,4 +1,5 @@
 import { polyfillDispose } from './dispose';
+import { polyfillIteratorHelpers } from './iterator-helpers';
 import { polyfillPromise } from './promise-with-resolvers';
 import { polyfillEventLoop } from './request-idle-callback';
 import { polyfillResizeObserver } from './resize-observer';
@@ -7,3 +8,4 @@ polyfillResizeObserver();
 polyfillEventLoop();
 await polyfillPromise();
 await polyfillDispose();
+await polyfillIteratorHelpers();
