5e51018c03fb3acb37ebd5c23c53e6fafa3febd0
Saul-Mirone <Saul-Mirone@outlook.com>
2025-01-07 08:48:05 +0000

    fix(editor): remove ts ignore (#9567)

--- blocksuite/affine/all/src/store/index.ts
+++ blocksuite/affine/all/src/store/index.ts
@@ -1,5 +1,3 @@
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 
-// oxlint-disable-next-line
-// @ts-ignore FIXME: typecheck error
 export * from '@blocksuite/store';
--- blocksuite/framework/store/src/index.ts
+++ blocksuite/framework/store/src/index.ts
@@ -10,28 +10,21 @@ export * from './transformer';
 export { type IdGenerator, nanoid, uuidv4 } from './utils/id-generator';
 export * from './yjs';
 
-const env =
+const env = (
   typeof globalThis !== 'undefined'
     ? globalThis
     : typeof window !== 'undefined'
       ? window
-      : // oxlint-disable-next-line
-        // @ts-ignore FIXME: typecheck error
-        typeof global !== 'undefined'
-        ? // oxlint-disable-next-line
-          // @ts-ignore FIXME: typecheck error
-          global
-        : {};
+      : typeof global !== 'undefined'
+        ? global
+        : {}
+) as Record<string, boolean>;
 const importIdentifier = '__ $BLOCKSUITE_STORE$ __';
 
-// oxlint-disable-next-line
-// @ts-ignore FIXME: typecheck error
 if (env[importIdentifier] === true) {
   // https://github.com/yjs/yjs/issues/438
   console.error(
     '@blocksuite/store was already imported. This breaks constructor checks and will lead to issues!'
   );
 }
-// oxlint-disable-next-line
-// @ts-ignore FIXME: typecheck error
 env[importIdentifier] = true;
